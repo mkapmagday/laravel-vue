144ae2cf4d72e4cdb5a8dfd20f2355209ef76e9d
Kmagday <kmagday9@gmail.com>
2023-03-05 21:22:13 +0800

    update

--- resources/js/components/Carousel.vue
+++ resources/js/components/Carousel.vue
@@ -1,44 +1,79 @@
 <template>
-  <carousel-3d :perspective="2000" :space="500" class="carousel">
-    <slide :index="0">
-      <img :src="require('../../images/assets/1.jpg')" alt="Slide 1" class="slide">
-    </slide>
-    <slide :index="1">
-      <img :src="require('../../images/assets/2.jpg')" alt="Slide 1" class="slide">
-    </slide>
-    <slide :index="2">
-      <img :src="require('../../images/assets/3.jpg')" alt="Slide 1" class="slide">
-    </slide>
-    <slide :index="3">
-      <img :src="require('../../images/assets/4.jpg')" alt="Slide 1" class="slide">
-    </slide>
-    <slide :index="4">
-      <img :src="require('../../images/assets/5.jpg')" alt="Slide 1" class="slide">
-    </slide>
-    <slide :index="5">
-      <img :src="require('../../images/assets/6.jpg')" alt="Slide 1" class="slide">
-    </slide>
-  </carousel-3d>
+  <div>
+    <carousel-3d :space="500" @afterChange="updateCurrentSlide">
+      <slide :index="0">
+        <img :src="require('../../images/assets/1.jpg')" alt="Slide 1">
+      </slide>
+      <slide :index="1">
+        <img :src="require('../../images/assets/2.jpg')" alt="Slide 2">
+      </slide>
+      <slide :index="2">
+        <img :src="require('../../images/assets/3.jpg')" alt="Slide 3">
+      </slide>
+      <slide :index="3">
+        <img :src="require('../../images/assets/4.jpg')" alt="Slide 4">
+      </slide>
+      <slide :index="4">
+        <img :src="require('../../images/assets/5.jpg')" alt="Slide 5">
+      </slide>
+      <slide :index="5">
+        <img :src="require('../../images/assets/6.jpg')" alt="Slide 6">
+      </slide>
+    </carousel-3d>
+    <div class="indicator">
+      <span v-for="i in slidesCount" :key="i" :class="{ active: i === currentSlide }" @click="goToSlide(i)"></span>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script>
+import { Carousel3d, Slide } from 'vue-carousel-3d';
+
+export default {
+  components: {
+    Carousel3d,
+    Slide,
+  },
+  data() {
+    return {
+      currentSlide: 1,
+      slidesCount: 6,
+    };
+  },
+  methods: {
+    updateCurrentSlide(index) {
+      this.currentSlide = index + 1;
+    },
+    goToSlide(index) {
+      this.currentSlide = index;
+      this.$refs.carousel3d.goToSlide(index - 1);
+    },
+  },
+};
+</script>
+
+<style>
 .carousel {
   max-width: 800px;
   margin: 0 auto;
 }
-
-.slide {
-  width: 100%;
-  height: 400px;
-  border-radius: 10px;
+.indicator {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
 }
 
-.carousel-3d-slide img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  border-radius: 10px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+.indicator span {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: #ccc;
+  margin: 0 10px;
+  cursor: pointer;
+}
+
+.indicator span.active {
+  background-color: #333;
 }
 
 </style>
